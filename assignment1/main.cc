d5a3be4730337cca7b6821f39490918b84768ecf
Peter <keifa.peter@gmail.com>
2017-02-10 15:03:27 +0100

    Can read from first arg

--- assignment1/main.cc
+++ assignment1/main.cc
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 #include "binary.tab.hh"
 #include "Node.h"
 extern Node root;
+extern FILE *yyin;
 
 void yy::parser::error(std::string const&err) {
   std::cout << "It's one of the bad ones... " << err << std::endl;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
+
+  yyin = fopen(argv[1], "r");
   yy::parser parser;
   if(!parser.parse()) {
     root.dump();
